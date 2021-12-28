c50f44df90341fa1fba514afe11ee369e4ee040f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-29 00:43:42 +0900

    store: home modules setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,10 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+// modules
+import home from '@/store/home.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: { home },
 });
