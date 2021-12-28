64ab25a4f92fc7a58351379271f41f607d514379
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-29 00:44:02 +0900

    store: home module checkList state setup

---
+++ src/store/home.js
@@ -0,0 +1,37 @@
+import money from '@/assets/money.png';
+import food from '@/assets/food.png';
+import gift from '@/assets/gift.png';
+import worry from '@/assets/worry.png';
+
+const state = {
+  checkList: [
+    {
+      keys: 1,
+      image: gift,
+      title1: '친구 선물',
+      title2: '이거 어때요?',
+    },
+    {
+      keys: 2,
+      image: food,
+      title1: '오늘 점심은',
+      title2: '이게 좋아요!',
+    },
+    {
+      keys: 3,
+      image: money,
+      title1: '축의금은',
+      title2: '이정도 내세요.',
+    },
+    {
+      keys: 4,
+      image: worry,
+      title1: '고민을',
+      title2: '공유해요.',
+    },
+  ],
+};
+
+export default {
+  state,
+};
