b636854f683bf1cac371a5b0288ba46f21094031
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-28 23:56:37 +0900

    choice component setup

---
+++ src/components/home/Choice.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="main-wrapper">
+    <div v-if="isLoading" class="splash-wrapper">
+      <Splash />
+    </div>
+    <div v-else>
+      <ChoiceItem v-for="item in checkList" :key="item.keys" :item="item" />
+    </div>
+  </div>
+</template>
+<script>
+import Splash from '@/components/home/Splash.vue';
+import ChoiceItem from '@/components/home/ChoiceItem.vue';
+import money from '@/assets/gift.png';
+import food from '@/assets/food.png';
+import gift from '@/assets/gift.png';
+import worry from '@/assets/worry.png';
+export default {
+  components: { Splash, ChoiceItem },
+  data() {
+    return {
+      isLoading: false,
+      checkList: [
+        {
+          keys: 1,
+          image: gift,
+          title1: '친구 선물',
+          title2: '이거 어때요?',
+        },
+        {
+          keys: 2,
+          image: food,
+          title1: '오늘 점심은',
+          title2: '이게 좋아요!',
+        },
+        {
+          keys: 3,
+          image: money,
+          title1: '축의금은',
+          title2: '이정도 내세요.',
+        },
+        {
+          keys: 4,
+          image: worry,
+          title1: '고민을',
+          title2: '공유해요.',
+        },
+      ],
+    };
+  },
+  methods: {
+    visit() {
+      this.isLoading = true;
+    },
+  },
+  mounted() {
+    this.visit();
+  },
+  watch: {
+    isLoading() {
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1500);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.main-wrapper {
+  width: 100%;
+  height: 100vh;
+  background: {
+    color: $main-color;
+  }
+}
+</style>
---
+++ src/components/home/ChoiceItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="item-wrapper button-gradient">
+    <img :src="item.image" alt="money" />
+    <div>
+      <p>{{ item.title1 }}</p>
+      <p>{{ item.title2 }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    item: Object,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.item-wrapper {
+  width: 163px;
+  height: 260px;
+  border: 1px solid #0da100;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.item-wrapper img {
+  width: calc(100% - 100px);
+  height: auto;
+}
+.item-wrapper p {
+  font-weight: $font-bold;
+  font-size: $font-h3-text;
+}
+.item-wrapper div p:nth-child(2) {
+  margin: {
+    top: 8px;
+  }
+}
+.item-wrapper div {
+  text-align: center;
+  margin: {
+    top: 34px;
+  }
+}
+</style>
