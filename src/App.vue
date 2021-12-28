b249099835cac56f409fb36dbd0764b8b0817cf9
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-28 23:57:13 +0900

    Add to div id view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="view">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
@@ -13,16 +11,9 @@
   font-family: $body-font-family;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#view {
+  max-width: 424px;
+  margin: 0 auto;
+  overflow: hidden;
 }
 </style>
