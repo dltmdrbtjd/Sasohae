fadd24bb263124b72f8ad0850a78eec7f8e936f0
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-28 23:56:02 +0900

    Add main choice page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Choice />
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Choice from '@/components/home/Choice.vue';
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { Choice },
 };
 </script>
