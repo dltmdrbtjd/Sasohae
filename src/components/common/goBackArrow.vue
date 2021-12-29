25d5f4f2cea6ad8d63464bd305cbcf891aaf3c1f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-30 01:24:17 +0900

    Add goBack Button

---
+++ src/components/common/goBackArrow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img src="@/assets/leftArrow.png" alt="goBack" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+div {
+  width: 100%;
+  height: 80px;
+  padding: 0 20px;
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  display: flex;
+  align-items: center;
+  > img {
+    width: 36px;
+    height: 36px;
+    cursor: pointer;
+  }
+}
+</style>
