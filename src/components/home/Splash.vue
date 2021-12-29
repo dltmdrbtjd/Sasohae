1776def5c4ccab0f4e833becd31a95db1f2a3ea7
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-30 01:26:15 +0900

    scss convention change

--- src/components/home/Splash.vue
+++ src/components/home/Splash.vue
@@ -27,47 +27,53 @@
   position: absolute;
   bottom: 0px;
   z-index: 2;
-}
-.footer > div {
-  width: 100%;
-  height: 6px;
-  position: absolute;
-  bottom: 34px;
-  left: 65%;
-}
-.footer > div > span {
-  display: inline-block;
-  width: 36px;
-  height: 6px;
-  margin-left: 10px;
-  background: {
-    color: red;
+
+  > div {
+    width: 100%;
+    height: 6px;
+    position: absolute;
+    bottom: 34px;
+    left: 65%;
   }
-}
-.footer > div > span:nth-child(1) {
-  background: {
-    color: #ffffb7;
+
+  > div > span {
+    display: inline-block;
+    width: 36px;
+    height: 6px;
+    margin-left: 10px;
+    background: {
+      color: red;
+    }
   }
-}
-.footer > div > span:nth-child(2) {
-  background: {
-    color: #fbfefa;
+
+  > div > span:nth-child(1) {
+    background: {
+      color: #ffffb7;
+    }
   }
-}
-.footer > span:nth-child(2) {
-  display: block;
-  width: 100%;
-  height: 16px;
-  background: {
-    color: $normal-brown-color;
+
+  > div > span:nth-child(2) {
+    background: {
+      color: #fbfefa;
+    }
   }
-}
-.footer > span:nth-child(3) {
-  display: block;
-  width: 100%;
-  height: 9px;
-  background: {
-    color: $soft-brown-color;
+
+  > span:nth-child(2) {
+    display: block;
+    width: 100%;
+    height: 16px;
+    background: {
+      color: $normal-brown-color;
+    }
+  }
+
+  > span:nth-child(3) {
+    display: block;
+    width: 100%;
+    height: 9px;
+    background: {
+      color: $soft-brown-color;
+    }
   }
 }
 </style>
