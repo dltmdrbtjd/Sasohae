194c830b5d840584666bf4aec45819ba41ad4bf1
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-30 01:24:40 +0900

    Add yes or no button

---
+++ src/components/common/OXButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <span class="button" @click="moveNextPage">
+    <p>{{ title }}</p>
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+  },
+  methods: {
+    moveNextPage() {
+      this.$store.commit('changePageNum', 1);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.button {
+  width: 100%;
+  padding: 16px 0;
+  text-align: center;
+  border: 1px solid $main-color;
+  border-radius: 30px;
+  margin-bottom: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background: {
+      color: $main-color;
+    }
+    color: $white-color;
+  }
+}
+</style>
