8a39d60cfae50b6c1a1a4d550534d5747fdab457
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-31 02:52:39 +0900

    Add new page routers

--- src/router/index.js
+++ src/router/index.js
@@ -14,14 +14,78 @@ const routes = [
     path: '/gift',
     name: 'Gift',
     component: () =>
-      import(/* webpackChunkName: "gift" */ '@/views/GiftPage.vue'),
+      import(/* webpackChunkName: "gift" */ '@/views/gift/GiftPage.vue'),
   },
   {
     path: '/gift-choice',
     name: 'GiftChoice',
     component: () =>
       import(
-        /* webpackChunkName: "gift-choice" */ '@/views/GiftChoicePage.vue'
+        /* webpackChunkName: "gift-choice" */ '@/views/gift/GiftChoicePage.vue'
+      ),
+  },
+  {
+    path: '/gift-random',
+    name: 'GiftRandom',
+    component: () =>
+      import(
+        /* webpackChunkName: "gift-random" */ '@/views/gift/GiftRandomPage.vue'
+      ),
+  },
+  {
+    path: '/gift-ranking',
+    name: 'GiftRanking',
+    component: () =>
+      import(
+        /* webpackChunkName: "gift-ranking" */ '@/views/gift/GiftRankingPage.vue'
+      ),
+  },
+  {
+    path: '/food',
+    name: 'FoodCategory',
+    component: () =>
+      import(
+        /* webpackChunkName: "food-category" */ '@/views/food/FoodCategoryPage.vue'
+      ),
+  },
+  {
+    path: '/food-choice',
+    name: 'FoodChoice',
+    component: () =>
+      import(
+        /* webpackChunkName: "food-choice" */ '@/views/food/FoodChoicePage.vue'
+      ),
+  },
+  {
+    path: '/food-recommend',
+    name: 'FoodRecommend',
+    component: () =>
+      import(
+        /* webpackChunkName: "food-recommend" */ '@/views/food/FoodRecommendPage.vue'
+      ),
+  },
+  {
+    path: '/food-ranking',
+    name: 'FoodRanking',
+    component: () =>
+      import(
+        /* webpackChunkName: "food-ranking" */ '@/views/food/FoodRankingPage.vue'
+      ),
+  },
+  {
+    path: '/money',
+    name: 'MoneyQuestion',
+    component: () =>
+      import(
+        /* webpackChunkName: "money-question" */ '@/views/money/MoneyQuestionPage.vue'
+      ),
+  },
+  {
+    path: '/worry',
+    name: 'WorryBoard',
+    component: () =>
+      import(
+        /* webpackChunkName: "worry-board" */ '@/views/worry/WorryBoardPage.vue'
       ),
   },
 ];
