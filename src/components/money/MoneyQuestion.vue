7416b99ee24e02c0647ca9779b7bf057b7bab2e0
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-31 02:51:53 +0900

    Money Component,page view success

---
+++ src/components/money/MoneyQuestion.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>최근 1년간 청첩장이 아닌 이유로 만난 적이 있다.</h2>
+    <div>
+      <button>네</button>
+      <button>아니오.</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+@include questionTitle(135px);
+div {
+  width: 100%;
+}
+
+h2 {
+  line-height: 2rem;
+}
+
+button {
+  width: 100%;
+  padding: 16px 0;
+  text-align: center;
+  border: 1px solid $main-color;
+  border-radius: 30px;
+  margin-bottom: 20px;
+  background: {
+    color: rgba(0, 0, 0, 0);
+  }
+  color: $main-color;
+  cursor: pointer;
+
+  &:hover {
+    background: {
+      color: $main-color;
+    }
+    color: $white-color;
+  }
+}
+</style>
---
+++ src/views/money/MoneyQuestionPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <MoneyQuestion />
+  </div>
+</template>
+<script>
+import MoneyQuestion from '@/components/money/MoneyQuestion.vue';
+export default {
+  components: { MoneyQuestion },
+};
+</script>
+<style lang="scss" scoped>
+div {
+  display: flex;
+  padding: 0 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
