0b1c37e1c4938728de5f95ee06804f53f04576c8
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-01 02:32:11 +0900

    get gift question actions create

--- src/store/gift.js
+++ src/store/gift.js
@@ -1,10 +1,28 @@
+import $http from 'axios';
 const state = {
   pageNum: 0,
+  giftQuestion: {},
+  giftAnswer: {
+    giftTarget: '',
+    giftEvent: '',
+    sex: '',
+    age: '',
+    giftAnswerExpensive: '',
+    giftAnswerPersonality: [],
+    giftAnswerEmotional: [],
+    giftAnswerTrendy: [],
+  },
 };
 
 const getters = {
   currentPageNum(state) {
     return state.pageNum;
+  },
+  giftQuestions(state) {
+    return state.giftQuestion || {};
+  },
+  giftAnswers(state) {
+    return state.giftAnswer || {};
   },
 };
 
@@ -12,10 +30,29 @@ const mutations = {
   changePageNum(state, pageNum) {
     state.pageNum += pageNum;
   },
+  getGiftQuestion(state, question) {
+    state.giftQuestion = question;
+  },
+  addGiftAnswer(state, request) {
+    state.giftAnswer[request.key] = request.answer;
+  },
+};
+
+const actions = {
+  async getQuestion({ commit }) {
+    try {
+      const resp = await $http.get('/gifts');
+      const question = resp.data;
+      commit('getGiftQuestion', question);
+    } catch (e) {
+      throw Error(e);
+    }
+  },
 };
 
 export default {
   state,
   getters,
   mutations,
+  actions,
 };
