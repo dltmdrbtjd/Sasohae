b818b5415a56e6803dbccbfdf0786c8f75f7dfbc
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-01 02:34:11 +0900

    create gift mixins for giftAnswer in vuex gift store

---
+++ src/mixins/GiftMixins.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  methods: {
+    giftAnswer(key, answer) {
+      if (answer === '네') {
+        answer = 'T';
+      } else if (answer === '아니오') {
+        answer = 'F';
+      } else if (answer === '잘 모르겠어요.') {
+        answer = '*';
+      }
+      const request = {
+        key,
+        answer,
+      };
+      this.$store.commit('addGiftAnswer', request);
+    },
+  },
+};
+</script>
