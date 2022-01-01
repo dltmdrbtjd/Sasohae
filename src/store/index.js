d8f3b987831895652e3020f801075cc34680415e
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-02 02:24:27 +0900

    add to food store

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import Vuex from 'vuex';
 // modules
 import home from '@/store/home.js';
 import gift from '@/store/gift.js';
+import food from '@/store/food.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { home, gift },
+  modules: { home, gift, food },
 });
