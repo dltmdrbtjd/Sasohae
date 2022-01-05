b3924548540d7ff154f632e73253de90e8ba96f7
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-05 22:18:13 +0900

    보드 랭킹 목록 애니메이션 추가

--- src/components/food/FoodCategory.vue
+++ src/components/food/FoodCategory.vue
@@ -15,12 +15,36 @@
         </div>
       </div>
     </div>
-    <span class="ranking-board"></span>
+    <div class="ranking-board">
+      <span>
+        <p>#{{ ranking[0].title }}</p>
+        <p>#{{ ranking[3].title }}</p>
+      </span>
+      <span>
+        <p>#{{ ranking[1].title }}</p>
+        <p>#{{ ranking[5].title }}</p>
+        <p>#{{ ranking[7].title }}</p>
+      </span>
+      <span>
+        <p>#{{ ranking[2].title }}</p>
+        <p>#{{ ranking[4].title }}</p>
+      </span>
+      <span>
+        <p>#{{ ranking[6].title }}</p>
+        <p>#{{ ranking[8].title }}</p>
+        <p>#{{ ranking[9].title }}</p>
+      </span>
+    </div>
     <img src="@/assets/character.png" alt="character" />
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      ranking: [],
+    };
+  },
   methods: {
     moveFoodChoice() {
       this.$router.push('food-choice');
@@ -28,6 +52,17 @@ export default {
     moveFoodRanking() {
       this.$router.push('food-ranking');
     },
+    async getMenuRanking() {
+      try {
+        const resp = await this.$http.get('/menu/ranking');
+        this.ranking = resp.data;
+      } catch (e) {
+        throw Error(e);
+      }
+    },
+  },
+  mounted() {
+    this.getMenuRanking();
   },
 };
 </script>
@@ -76,10 +111,62 @@ export default {
     margin: {
       top: 17px;
     }
+    padding: 30px;
     border: 8px solid $hard-brown-color;
     box-sizing: border-box;
     background: {
       color: $main-color;
+    }
+
+    > span {
+      width: 400px;
+      display: flex;
+      justify-content: space-around;
+      align-content: center;
+      margin-bottom: 10px;
+      color: $white-color;
+      -webkit-animation-name: movingPara2;
+      -webkit-animation-duration: 5.5s;
+      animation-name: movingPara;
+      animation-duration: 5.5s;
+      -webkit-animation-iteration-count: infinite;
+      animation-iteration-count: infinite;
+
+      &:nth-child(2) {
+        width: 600px;
+        animation-duration: 5s;
+        -webkit-animation-duration: 5s;
+      }
+      &:nth-child(3) {
+        width: 400px;
+        animation-duration: 4s;
+        -webkit-animation-duration: 4s;
+        -webkit-animation-name: movingPara2;
+        animation-name: movingPara2;
+      }
+      &:nth-child(4) {
+        width: 600px;
+        animation-duration: 6s;
+        -webkit-animation-duration: 6s;
+      }
+    }
+
+    @keyframes movingPara {
+      from {
+        margin-left: 80%;
+      }
+      to {
+        margin-left: -250%;
+      }
+    }
+
+    @keyframes movingPara2 {
+      from {
+        margin-left: 130%;
+      }
+      to {
+        margin-left: -200%;
+      }
     }
   }
 
--- src/components/gift/GiftCategory.vue
+++ src/components/gift/GiftCategory.vue
@@ -16,12 +16,39 @@
       </div>
     </div>
     <div @click="moveGiftRanking" class="ranking">실시간 랭킹 순위</div>
-    <span class="ranking-board"></span>
+    <div class="ranking-board">
+      <span>
+        <p>#{{ ranking[0].title }}</p>
+        <p>#{{ ranking[3].title }}</p>
+      </span>
+      <span>
+        <p>#{{ ranking[1].title }}</p>
+        <p>#{{ ranking[5].title }}</p>
+        <p>#{{ ranking[7].title }}</p>
+      </span>
+      <span>
+        <p>#{{ ranking[2].title }}</p>
+        <p>#{{ ranking[4].title }}</p>
+      </span>
+      <span>
+        <p>#{{ ranking[6].title }}</p>
+        <p>#{{ ranking[8].title }}</p>
+        <p>#{{ ranking[9].title }}</p>
+      </span>
+    </div>
     <img src="@/assets/character.png" alt="character" />
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      ranking: [],
+    };
+  },
+  mounted() {
+    this.getGiftRanking();
+  },
   methods: {
     moveGiftChoice() {
       this.$router.push('gift-choice');
@@ -31,6 +58,14 @@ export default {
     },
     moveGiftRanking() {
       this.$router.push('gift-ranking');
+    },
+    async getGiftRanking() {
+      try {
+        const resp = await this.$http.get('/gifts/ranking');
+        this.ranking = resp.data;
+      } catch (e) {
+        throw Error(e);
+      }
     },
   },
 };
@@ -94,10 +129,62 @@ export default {
     margin: {
       top: 17px;
     }
+    padding: 30px;
     border: 8px solid $hard-brown-color;
     box-sizing: border-box;
     background: {
       color: $main-color;
+    }
+
+    > span {
+      width: 400px;
+      display: flex;
+      justify-content: space-around;
+      align-content: center;
+      margin-bottom: 10px;
+      color: $white-color;
+      -webkit-animation-name: movingPara2;
+      -webkit-animation-duration: 5.5s;
+      animation-name: movingPara;
+      animation-duration: 5.5s;
+      -webkit-animation-iteration-count: infinite;
+      animation-iteration-count: infinite;
+
+      &:nth-child(2) {
+        width: 600px;
+        animation-duration: 5s;
+        -webkit-animation-duration: 5s;
+      }
+      &:nth-child(3) {
+        width: 400px;
+        animation-duration: 4s;
+        -webkit-animation-duration: 4s;
+        -webkit-animation-name: movingPara2;
+        animation-name: movingPara2;
+      }
+      &:nth-child(4) {
+        width: 600px;
+        animation-duration: 6s;
+        -webkit-animation-duration: 6s;
+      }
+    }
+
+    @keyframes movingPara {
+      from {
+        margin-left: 80%;
+      }
+      to {
+        margin-left: -250%;
+      }
+    }
+
+    @keyframes movingPara2 {
+      from {
+        margin-left: 130%;
+      }
+      to {
+        margin-left: -200%;
+      }
     }
   }
 
