41195927953ffd936ae6a0fe812194923521ce59
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-06 23:34:21 +0900

    blur box responsive fix

--- src/components/common/BlurBox.vue
+++ src/components/common/BlurBox.vue
@@ -26,27 +26,55 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.blur-box {
-  position: fixed;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 375px;
-  height: 100vh;
-  background-color: rgba(44, 44, 44, 0.7);
-  backdrop-filter: blur(4px);
-  z-index: 9;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+@media (min-width: 501px) {
+  .blur-box {
+    position: fixed;
+    top: 0;
+    left: 65%;
+    transform: translateX(-50%);
+    width: 375px;
+    height: 100vh;
+    background-color: rgba(44, 44, 44, 0.7);
+    backdrop-filter: blur(4px);
+    z-index: 9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  > img {
-    width: 36px;
-    height: 36px;
-    cursor: pointer;
-    position: absolute;
-    top: 20px;
-    left: 20px;
+    > img {
+      width: 36px;
+      height: 36px;
+      cursor: pointer;
+      position: absolute;
+      top: 20px;
+      left: 20px;
+    }
+  }
+}
+
+@media (max-width: 500px) {
+  .blur-box {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 375px;
+    height: 100vh;
+    background-color: rgba(44, 44, 44, 0.7);
+    backdrop-filter: blur(4px);
+    z-index: 9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > img {
+      width: 36px;
+      height: 36px;
+      cursor: pointer;
+      position: absolute;
+      top: 20px;
+      left: 20px;
+    }
   }
 }
 </style>
