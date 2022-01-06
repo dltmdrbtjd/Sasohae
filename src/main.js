e3f36a4b4c86a92f405d6c3094e74323b8d392b6
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-06 22:48:44 +0900

    connect kakao sdk with apikey

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueKakaoSdk from 'vue-kakao-sdk';
 import router from './router';
 import store from './store';
 import './plugins';
 
+const apiKey = process.env.VUE_APP_KAKAO;
+Vue.use(VueKakaoSdk, { apiKey });
 Vue.config.productionTip = false;
 
 new Vue({
