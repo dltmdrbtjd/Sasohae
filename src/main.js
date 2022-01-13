2b2272405ecb0cbcc3fb80d262b3812a7a6205db
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-13 19:33:28 +0900

    google analytics connect to vue-gtag

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import App from './App.vue';
 import VueKakaoSdk from 'vue-kakao-sdk';
 import router from './router';
 import store from './store';
+import VueGtag from 'vue-gtag';
 import './plugins';
 
 const apiKey = process.env.VUE_APP_KAKAO;
+const googleTrackingId = process.env.VUE_APP_TRACKING_ID;
+Vue.use(
+  VueGtag,
+  {
+    config: {
+      id: googleTrackingId,
+      params: {
+        send_page_view: false,
+      },
+    },
+  },
+  router,
+);
 Vue.use(VueKakaoSdk, { apiKey });
 Vue.config.productionTip = false;
 
