e76dd59a0e4fd900baaa5649a5d3d5d46f33709b
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-16 01:55:19 +0900

    vue,babel jest, vue storybook setup

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'vue'],
   transform: {
-    '^.+\\.(js|jsx)?$': 'babel-jest',
+    '.*\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    'src/(.+)$/': '<rootDir>/src/$1',
+    '@/(.*)': '<rootDir>/src/$1',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
   },
-  testMatch: [
-    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
-    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
-  ],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  transformIgnorePatterns: ['/node_modules/'],
 };
