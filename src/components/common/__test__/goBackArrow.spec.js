d8a84382763725af0cb13b543f4d535f801da34d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-17 19:57:00 +0900

    goBackArrow isGiftPage func false test case

--- src/components/common/__test__/goBackArrow.spec.js
+++ src/components/common/__test__/goBackArrow.spec.js
@@ -10,4 +10,9 @@ describe('goBackArrowComponent', () => {
     const localThis = localData(8);
     expect(GoBackArrow.computed.isGiftPage.call(localThis)).toBeFalsy();
   });
+
+  it('it should return true', () => {
+    const localThis = localData(7);
+    expect(GoBackArrow.computed.isGiftPage.call(localThis)).toBeTruthy();
+  });
 });
