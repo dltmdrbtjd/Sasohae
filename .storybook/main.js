68989779de629b462a3fb8d10ad10ceca3b80046
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-18 22:30:48 +0900

    sass-loader version up for storybook wepback sass loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,60 @@
+const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue"
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/vue',
+
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.sass$/,
+      use: [
+        'style-loader',
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^9.0.0
+          options: {
+            // This is the path to your variables
+            additionalData: `
+              @import '@/styles/variables.scss'
+            `,
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^9.0.0
+          options: {
+            // This is the path to your variables
+            additionalData: `
+              @import '@/styles/variables.scss';
+            `,
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    config.module.rules.push({
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+          vue: 'vue/dist/vue.js',
+          vue$: 'vue/dist/vue.esm.js',
+        },
+      },
+    });
+
+    // Return the altered config
+    return config;
+  },
+};
